7c32cdd88cb672180c5bfd04d0d82f2d9f4519ed
Samir Omarov <samir.omarov@gmail.com>
2021-12-03 01:27:53 +0300

    add create & read restAPI with tests

--- project/app/main.py
+++ project/app/main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+from typing import Optional, List
 
 from app.config import get_settings, Settings
 
 
-app = FastAPI()
+class Todo(BaseModel):
+    id: int
+    title: str
+
+
+app = FastAPI(title="TDD Todo API")
+
+store_todo = []
 
 
 @app.get("/ping")
@@ -13,3 +22,14 @@ async def pong(settings: Settings = Depends(get_settings)):
         "environment": settings.environment,
         "testing": settings.testing,
     }
+
+
+@app.post("/todo/", status_code=201)
+async def create_todo(todo: Todo):
+    store_todo.append(todo)
+    return todo
+
+@app.get("/todo/", response_model=List[Todo])
+async def get_all_todos():
+    return store_todo
+
---
+++ project/tests/test_todos.py
@@ -0,0 +1,39 @@
+import json
+
+import pytest
+
+
+def test_create_todo(test_app):
+    response = test_app.post("/todo/", data=json.dumps({"id": 2, "title": "TDD Test"}))
+    assert response.status_code == 201
+    assert response.json()["title"] == "TDD Test"
+
+
+def test_create_todo_invalid_json(test_app):
+    response = test_app.post("/todo/", data=json.dumps({}))
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "id"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+            {
+                "loc": ["body", "title"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+        ]
+    }
+
+
+def test_get_all_todos(test_app):
+    response = test_app.post("/todo/", data=json.dumps({"id": 1, "title": "TDD Test"}))
+    todo_id = response.json()["id"]
+
+    response = test_app.get("/todo/")
+    assert response.status_code == 200
+
+    response_list = response.json()
+    assert len(list(filter(lambda d: d["id"] == todo_id, response_list))) == 1
