e44df8c90dbd95236bf753412f3014c0791025ea
Samir Omarov <samir.omarov@gmail.com>
2021-12-03 15:52:14 +0300

    update pyTest

--- project/tests/test_todos.py
+++ project/tests/test_todos.py
@@ -24,10 +24,5 @@ def test_create_todo_invalid_json(test_app):
 
 def test_get_all_todos(test_app):
     response = test_app.post("/todo/", data=json.dumps({"title": "TDD Test"}))
-    todo_id = response.json()["id"]
-
-    response = test_app.get("/todo/")
     assert response.status_code == 200
-
-    response_list = response.json()
-    assert len(list(filter(lambda d: d["id"] == todo_id, response_list))) == 1
+    assert response.json() == {"title": "TDD Test"}
